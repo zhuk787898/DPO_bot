d26682998b771489cabf696ee5b7fd3f1ae8de58
Yaroclav Zhukov <yazhukov@mai.education>
2024-06-06 10:03:29 +0300

    новая дичь

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import multiprocessing
 import subprocess
 import click
-import os
-from pathlib import Path
+import platform
 
 # Функция для запуска процессов в новом окне консоли (только для Windows)
 def run_in_new_console(command):
-    subprocess.run(f'start cmd /K "{command}"', shell=True)
+    if platform.system() == "Windows":
+        subprocess.run(f'start cmd /K "{command}"', shell=True)
+    else:
+        raise NotImplementedError("Эта функция поддерживается только на Windows.")
 
 # Функция для запуска Telegram бот сервиса
 def run_telegram_bot_service():
@@ -20,11 +22,6 @@ def run_video_processing_service():
 def run_presentation_generation_service():
     run_in_new_console("uvicorn presentation_generator.generator:app --host 127.0.0.1 --port 8002")
 
-# Создание процессов для каждого сервиса
-telegram_bot_process = multiprocessing.Process(target=run_telegram_bot_service)
-video_processing_process = multiprocessing.Process(target=run_video_processing_service)
-presentation_generation_process = multiprocessing.Process(target=run_presentation_generation_service)
-
 @click.group()
 def main():
     pass
@@ -32,61 +29,71 @@ def main():
 @main.command()
 def start_all():
     """Запуск всех сервисов"""
-    if not telegram_bot_process.is_alive():
-        telegram_bot_process.start()
-    if not video_processing_process.is_alive():
-        video_processing_process.start()
-    if not presentation_generation_process.is_alive():
-        presentation_generation_process.start()
+    telegram_bot_process = multiprocessing.Process(target=run_telegram_bot_service)
+    video_processing_process = multiprocessing.Process(target=run_video_processing_service)
+    presentation_generation_process = multiprocessing.Process(target=run_presentation_generation_service)
 
+    telegram_bot_process.start()
+    video_processing_process.start()
+    presentation_generation_process.start()
+
+    telegram_bot_process.join()
+    video_processing_process.join()
+    presentation_generation_process.join()
 
 @main.command()
 def stop_all():
     """Остановка всех сервисов"""
-    if telegram_bot_process.is_alive():
-        telegram_bot_process.terminate()
-    if video_processing_process.is_alive():
-        video_processing_process.terminate()
-    if presentation_generation_process.is_alive():
-        presentation_generation_process.terminate()
+    for process in multiprocessing.active_children():
+        process.terminate()
 
 @main.command()
 def start_tg():
     """Запуск Telegram бот сервиса"""
-    if not telegram_bot_process.is_alive():
-        telegram_bot_process.start()
-
+    telegram_bot_process = multiprocessing.Process(target=run_telegram_bot_service)
+    telegram_bot_process.start()
+    telegram_bot_process.join()
 
 @main.command()
 def stop_tg():
     """Остановка Telegram бот сервиса"""
-    if telegram_bot_process.is_alive():
-        telegram_bot_process.terminate()
-
+    for process in multiprocessing.active_children():
+        if process.name == "Process-1":  # Название процесса по умолчанию для первого создаваемого процесса
+            process.terminate()
+            process.join(timeout=5)
+            if not process.is_alive():
+                print("Сервис Telegram бота успешно остановлен.")
+            else:
+                print("Не удалось остановить сервис Telegram бота.")
+                print("Попробуйте остановить сервис вручную.")
 
 @main.command()
 def start_video():
     """Запуск сервиса обработки видео"""
-    if not video_processing_process.is_alive():
-        video_processing_process.start()
+    video_processing_process = multiprocessing.Process(target=run_video_processing_service)
+    video_processing_process.start()
+    video_processing_process.join()
 
 @main.command()
 def stop_video():
     """Остановка сервиса обработки видео"""
-    if video_processing_process.is_alive():
-        video_processing_process.terminate()
+    for process in multiprocessing.active_children():
+        if process.name == "Process-2":  # Название процесса по умолчанию для второго создаваемого процесса
+            process.terminate()
 
 @main.command()
 def start_presentation():
     """Запуск сервиса генерации презентаций"""
-    if not presentation_generation_process.is_alive():
-        presentation_generation_process.start()
+    presentation_generation_process = multiprocessing.Process(target=run_presentation_generation_service)
+    presentation_generation_process.start()
+    presentation_generation_process.join()
 
 @main.command()
 def stop_presentation():
     """Остановка сервиса генерации презентаций"""
-    if presentation_generation_process.is_alive():
-        presentation_generation_process.terminate()
+    for process in multiprocessing.active_children():
+        if process.name == "Process-3":  # Название процесса по умолчанию для третьего создаваемого процесса
+            process.terminate()
 
 if __name__ == "__main__":
     main()
--- bot/bot.py
+++ bot/bot.py
@@ -9,106 +9,91 @@ from config import TELEGRAM_TOKEN, VIDEO_PROCESSING_SERVICE_URL, PRESENTATION_GE
 
 app = FastAPI()
 
-CHUNK_SIZE = 1024 * 1024  # 1 MB
-
-@app.post("/handle_video_chunk/")
-async def handle_video_chunk(file: UploadFile = File(...), chunk_number: int = Form(...), total_chunks: int = Form(...)):
-    file_location = f"temp_chunk_{chunk_number}_{file.filename}"
+@app.post("/handle_video/")
+async def handle_video(file: UploadFile = File(...)):
+    file_location = f"temp_{file.filename}"
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
 
-    if chunk_number == total_chunks:
-        # Merge chunks
-        merged_file_location = f"temp_{file.filename}"
-        with open(merged_file_location, "wb") as merged_file:
-            for i in range(1, total_chunks + 1):
-                chunk_file_location = f"temp_chunk_{i}_{file.filename}"
-                with open(chunk_file_location, "rb") as chunk_file:
-                    merged_file.write(chunk_file.read())
-                os.remove(chunk_file_location)
+    try:
+        async with httpx.AsyncClient(follow_redirects=True) as client:
+            print(f"Uploading video to {VIDEO_PROCESSING_SERVICE_URL}")
+            with open(file_location, 'rb') as video_file:
+                response = await client.post(VIDEO_PROCESSING_SERVICE_URL, files={'file': video_file})
+                print(f"Response from video processing service: {response.text}")
+                data = response.json()
 
-        try:
-            async with httpx.AsyncClient(follow_redirects=True) as client:
-                print(f"Uploading video to {VIDEO_PROCESSING_SERVICE_URL}")
-                with open(merged_file_location, 'rb') as video_file:
-                    response = await client.post(VIDEO_PROCESSING_SERVICE_URL, files={'file': video_file})
-                    print(f"Response from video processing service: {response.text}")
-                    data = response.json()
+            print(f"Sending data to {PRESENTATION_GENERATION_SERVICE_URL}")
+            response = await client.post(PRESENTATION_GENERATION_SERVICE_URL, json=data)
+            response.raise_for_status()
+            print(f"Response from presentation generation service: {response.text}")
+            presentation_file_url = response.json().get('presentation_url')
 
-                print(f"Sending data to {PRESENTATION_GENERATION_SERVICE_URL}")
-                response = await client.post(PRESENTATION_GENERATION_SERVICE_URL, json=data)
+        return {"presentation_url": presentation_file_url}
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e.response.status_code}, {e.response.text}")
+        return {"error": f"HTTP error occurred: {e.response.status_code}, {e.response.text}"}
+    except httpx.RequestError as e:
+        print(f"Request error occurred: {str(e)}")
+        return {"error": f"Request error occurred: {str(e)}"}
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return {"error": f"An error occurred: {str(e)}"}
+    finally:
+        os.remove(file_location)
+
+async def notify_video_upload(update: Update):
+    return await update.message.reply_text("Видео загружается на сервер. Пожалуйста, подождите...")
+
+async def notify_video_processing(update: Update):
+    return await update.message.reply_text("Видео успешно загружено и обрабатывается...")
+
+async def video_handler_bot(update: Update, context):
+    message = await notify_video_upload(update)
+    file = await update.message.video.get_file()
+    file_path = await file.download_to_drive()
+    
+    try:
+        async with httpx.AsyncClient(follow_redirects=True) as client:
+            print(f"Uploading video to bot request URL: {BOT_REQUEST_URL}")
+            with open(file_path, "rb") as video_file:
+                response = await client.post(BOT_REQUEST_URL, files={"file": video_file})
                 response.raise_for_status()
-                print(f"Response from presentation generation service: {response.text}")
-                presentation_file_url = response.json().get('presentation_url')
+                response_json = response.json()
+                print(f"Response from bot request URL: {response.text}")
 
-            return {"presentation_url": presentation_file_url}
-        except httpx.HTTPStatusError as e:
-            print(f"HTTP error occurred: {e.response.status_code}, {e.response.text}")
-            return {"error": f"HTTP error occurred: {e.response.status_code}, {e.response.text}"}
-        except httpx.RequestError as e:
-            print(f"Request error occurred: {str(e)}")
-            return {"error": f"Request error occurred: {str(e)}"}
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            return {"error": f"An error occurred: {str(e)}"}
-        finally:
-            os.remove(merged_file_location)
-    else:
-        return {"status": "chunk received"}
+        await notify_video_processing(update)
+
+        if "presentation_url" in response_json:
+            presentation_url = response_json["presentation_url"]
+            
+            await context.bot.send_document(
+                chat_id=update.effective_chat.id,
+                document=open(presentation_url, 'rb')
+            )
+            
+            os.remove(presentation_url)
+        else:
+            error_message = response_json.get('error', 'Unknown error occurred.')
+            await update.message.reply_text(f"Error: {error_message}")
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e.response.status_code}, {e.response.text}")
+        await update.message.reply_text(f"HTTP error occurred: {e.response.status_code}, {e.response.text}")
+    except httpx.RequestError as e:
+        print(f"Request error occurred: {str(e)}")
+        await update.message.reply_text(f"Request error occurred: {str(e)}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        await update.message.reply_text(f"An error occurred: {str(e)}")
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)
 
 async def start(update: Update, context):
     await update.message.reply_text('Салам! Кидай видос!')
 
 async def help(update: Update, context):
     await update.message.reply_text('Помощь!')
-
-async def video_handler_bot(update: Update, context):
-    message = await update.message.reply_text("Видео загружается на сервер. Пожалуйста, подождите...")
-    file = await update.message.video.get_file()
-    file_path = await file.download_to_drive()
-
-    file_size = os.path.getsize(file_path)
-    total_chunks = (file_size // CHUNK_SIZE) + 1
-
-    try:
-        async with httpx.AsyncClient(follow_redirects=True) as client:
-            for chunk_number in range(1, total_chunks + 1):
-                with open(file_path, "rb") as video_file:
-                    video_file.seek((chunk_number - 1) * CHUNK_SIZE)
-                    chunk = video_file.read(CHUNK_SIZE)
-                    files = {'file': ('chunk', chunk, 'video/mp4')}
-                    data = {'chunk_number': chunk_number, 'total_chunks': total_chunks}
-                    response = await client.post(BOT_REQUEST_URL, files=files, data=data)
-                    response.raise_for_status()
-
-                await message.edit_text(f"Прогресс загрузки: {int((chunk_number / total_chunks) * 100)}%")
-
-            response_json = response.json()
-            print(f"Response from bot request URL: {response.text}")
-
-        await update.message.reply_text("Видео успешно загружено и обрабатывается...")
-
-        if "presentation_url" in response_json:
-            presentation_url = response_json["presentation_url"]
-
-            await context.bot.send_document(
-                chat_id=update.effective_chat.id,
-                document=open(presentation_url, 'rb')
-            )
-
-            os.remove(presentation_url)
-        else:
-            error_message = response_json.get('error', 'Unknown error occurred.')
-            await update.message.reply_text(f"Error: {error_message}")
-    except httpx.HTTPStatusError as e:
-        await update.message.reply_text(f"HTTP error occurred: {e.response.status_code}, {e.response.text}")
-    except httpx.RequestError as e:
-        await update.message.reply_text(f"Request error occurred: {str(e)}")
-    except Exception as e:
-        await update.message.reply_text(f"An error occurred: {str(e)}")
-    finally:
-        if os.path.exists(file_path):
-            os.remove(file_path)
 
 def run_fastapi():
     uvicorn.run(app, host="127.0.0.1", port=8000)
